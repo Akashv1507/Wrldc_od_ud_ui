a445396299b0484f982b7b1243ea146da3ebd86b
Aakash Verma <akash.v@wrldc.local>
2021-12-16 16:49:52 +0530

    minor changes

--- setup_service.bat
+++ setup_service.bat
@@ -1,5 +1,5 @@
-call nssm.exe install soFarHighest "%cd%\run_server.bat"
-rem call nssm.exe edit soFarHighest
-call nssm.exe set soFarHighest AppStdout "%cd%\logs\soFarHighest.log"
-call nssm.exe set soFarHighest AppStderr "%cd%\logs\soFarHighest.log"
-call sc start soFarHighest
+call nssm.exe install mis_od_ud_ui "%cd%\run_server.bat"
+rem call nssm.exe edit mis_od_ud_ui
+call nssm.exe set mis_od_ud_ui AppStdout "%cd%\logs\mis_od_ud_ui.log"
+call nssm.exe set mis_od_ud_ui AppStderr "%cd%\logs\mis_od_ud_ui.log"
+call sc start mis_od_ud_ui

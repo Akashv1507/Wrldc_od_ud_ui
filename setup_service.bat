5a670ec6a72417d6c6a9c96140a3ddfa97ab285b
Aakash Verma <akash.v@wrldc.local>
2021-12-21 16:58:34 +0530

    ui sign corrected

--- setup_service.bat
+++ setup_service.bat
@@ -1,5 +1,5 @@
-call nssm.exe install mis_od_ud_ui "%cd%\run_server.bat"
-rem call nssm.exe edit mis_od_ud_ui
-call nssm.exe set mis_od_ud_ui AppStdout "%cd%\logs\mis_od_ud_ui.log"
-call nssm.exe set mis_od_ud_ui AppStderr "%cd%\logs\mis_od_ud_ui.log"
-call sc start mis_od_ud_ui
+call nssm.exe install mis_psp_scada "%cd%\run_server.bat"
+rem call nssm.exe edit mis_psp_scada
+call nssm.exe set mis_psp_scada AppStdout "%cd%\logs\mis_psp_scada.log"
+call nssm.exe set mis_psp_scada AppStderr "%cd%\logs\mis_psp_scada.log"
+call sc start mis_psp_scada

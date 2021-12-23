9fdbf4ad6a82f62d458604d96afe3546c5c75439
Aakash Verma <akash.v@wrldc.local>
2021-12-23 16:56:49 +0530

    back/next arrow addedd

--- static/src/schVsActDrawlPage/schVsActDrawl.ts
+++ static/src/schVsActDrawlPage/schVsActDrawl.ts
@@ -24,6 +24,17 @@ window.onload = async () => {
     searchResultLimit: 50,
     renderChoiceLimit: 50,
   });
+
+  const yesterday = ( d => new Date(d.setDate(d.getDate()-1)) )(new Date).toISOString().slice(0,10);
+
+  //setting startdate and enddate to yesterday
+  (document.getElementById("startDate") as HTMLInputElement).value= yesterday;
+  (document.getElementById("endDate") as HTMLInputElement).value = yesterday;
+
+  // getting right arrow btn and left arrow btn
+  (document.getElementById("leftArrow") as HTMLButtonElement).onclick = setPrevDate;
+  (document.getElementById("rightArrow") as HTMLButtonElement).onclick = setNextDate;
+  
   const submitBtn = document.getElementById("submitBtn") as HTMLButtonElement
   submitBtn.onclick= wrapperFunc;
 
@@ -33,6 +44,7 @@ window.onload = async () => {
   const plotSectionDiv = document.getElementById("schVsActDrawlPlotSection") as HTMLDivElement
   const tblSectionDiv = document.getElementById("schVsActDrawlTableSection") as HTMLDivElement
 
+ 
   chartIconBtn.classList.add("chartActive")
   tblSectionDiv.hidden =true
 
@@ -56,8 +68,47 @@ window.onload = async () => {
 
 const wrapperFunc = async ()=>{
      fetchPlotData()
-     fetchTableData()
-   
+     fetchTableData()   
 }
 
+const setPrevDate = ()=>{
+  
+  // console.log(this.id)
+  //return in string
+  let startDate=(document.getElementById("startDate") as HTMLInputElement)
+  let endDate = (document.getElementById("endDate") as HTMLInputElement)
+
+  //converting to date obj, then subtracting one day then setting to startdate and endDate value and calling both the functions
+  const startDateObj = new Date (startDate.value)
+  const endDateObj = new Date (endDate.value)
+
+  let newStartDate= startDateObj.setDate(startDateObj.getDate()-1)
+  startDate.value = (new Date(newStartDate)).toISOString().slice(0,10)
+
+  let newEndDate= endDateObj.setDate(endDateObj.getDate()-1)
+  endDate.value  = (new Date(newEndDate)).toISOString().slice(0,10)
+  
+  //calling both the function with the new start date and end date
+  fetchPlotData()
+  fetchTableData()
+}
+const setNextDate = ()=>{
+  //return in string
+  let startDate=(document.getElementById("startDate") as HTMLInputElement)
+  let endDate = (document.getElementById("endDate") as HTMLInputElement)
+
+  //converting to date obj, then subtracting one day then setting to startdate and endDate value and calling both the functions
+  const startDateObj = new Date (startDate.value)
+  const endDateObj = new Date (endDate.value)
+
+  let newStartDate= startDateObj.setDate(startDateObj.getDate()+1)
+  startDate.value = (new Date(newStartDate)).toISOString().slice(0,10)
+
+  let newEndDate= endDateObj.setDate(endDateObj.getDate()+1)
+  endDate.value  = (new Date(newEndDate)).toISOString().slice(0,10)
+  
+  //calling both the function with the new start date and end date
+  fetchPlotData()
+  fetchTableData()
+}
 
